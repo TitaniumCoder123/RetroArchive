bc43446388d52831389f989bc465a7281f0bd277
TitaniumCoder123 <titaniumcreeper2000@gmail.com>
2026-04-03 22:54:48 -0500

    Add files via upload

--- install_retroarchive.sh
+++ install_retroarchive.sh
@@ -17,8 +17,8 @@ chmod +x "/userdata/roms/ports/RetroArchive/retroarchive"
 
 # Create a launcher script for Batocera
 echo '#!/bin/bash
-cd "/userdata/roms/ports/RetroArchive"
-python RetroArchive' > "/userdata/roms/ports/workingapps.sh"
+chmod +x /userdata/roms/ports/workingapp/workingapps
+cd /userdata/roms/ports/RetroArchive/retroarchive/ && export DISPLAY=:0.0; ./retroarchive
 
 chmod +x "/userdata/roms/ports/RetroArchive.sh"
 
